16ed5589e7f5f5626d87d50f2e274324544fe8b6
Abdul Moqueet <Abdul-Moqueet@users.noreply.github.com>
2019-08-13 11:11:24 +0530

    added broadcast receiver

---
+++ app/src/main/java/com/moqueetabdul/findmyphone/IncomingSms.java
@@ -0,0 +1,47 @@
+package com.moqueetabdul.findmyphone;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+public class IncomingSms extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Toast.makeText(context, "received", Toast.LENGTH_SHORT).show();
+
+        final Bundle bundle = intent.getExtras();
+
+        try {
+
+            if (bundle != null) {
+
+                final Object[] pdusObj = (Object[]) bundle.get("pdus");
+
+                for (int i = 0; i < pdusObj.length; i++) {
+
+                    SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+                    String phoneNumber = currentMessage.getDisplayOriginatingAddress();
+
+                    String message = currentMessage.getDisplayMessageBody();
+
+                    Log.i("SmsReceiver", "senderNum: "+ phoneNumber + "; message: " + message);
+
+                    // Show Alert
+                    Toast toast = Toast.makeText(context, "senderNum: "+ phoneNumber + ", message: " + message, Toast.LENGTH_LONG);
+                    toast.show();
+
+                } // end for loop
+            } // bundle is null
+
+        } catch (Exception e) {
+            Log.e("SmsReceiver", "Exception smsReceiver" +e);
+        }
+    }
+
+}
